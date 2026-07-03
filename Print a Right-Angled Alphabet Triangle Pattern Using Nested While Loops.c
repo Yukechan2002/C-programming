70651d53faec2ed0ec033c5824d88fa16c91988b
Yukesh Chandran <167736831+Yukechan2002@users.noreply.github.com>
2026-07-04 00:44:06 +0530

    Create Print a Right-Angled Alphabet Triangle Pattern Using Nested While Loops.c

---
+++ Print a Right-Angled Alphabet Triangle Pattern Using Nested While Loops.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    
+    int i,j;
+    i=1;
+    int a = 65;
+    
+    while(i<=5){
+        j=1;
+       while(j<=i){
+           printf("%c ",a);
+           j+=1;
+           a+=1;
+       } 
+       printf("\n");
+       i+=1;
+    }
+    
+    return 0;
+}
